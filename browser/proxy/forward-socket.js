33f05f267475014875bfeb09dcd930b74bb2efa9
lachrist <lachrist@vub.ac.be>
2018-06-17 22:38:19 +0200

    correctect a few bug triggered when the target application uses upgrade http request

--- browser/proxy/forward-socket.js
+++ browser/proxy/forward-socket.js
@@ -10,14 +10,16 @@ module.exports = (event, {host, hostname, port, prefix, emitter}) => (request, s
     request.url = request.url.substring(prefix.length);
     emitter.emit(event, request, socket, head);
   } else {
-    const forward_socket = new (request.socket.encrypted ? Net : Tls).Socket();
-    forward_socket.on("error", OnError(event+"-forward-socket"));
+    let forward_socket = new Net.Socket();
+    if (request.socket.encrypted)
+      forward_socket = new Tls.TLSSocket(forward_socket);
+    forward_socket.on("error", OnError(event+"-forward-socket", emitter));
     forward_socket.on("connect", () => {
       forward_socket.write(request.method+" "+request.url+" HTTP/"+request.httpVersion+"\r\n");
       for (let index = 0, length = request.rawHeaders.length; index < length; index+=2)
-        forward_socket.write(request.rawHeaders[index]+": "+rawHeaders.rawHeaders[index+1]+"\r\n");
+        forward_socket.write(request.rawHeaders[index]+": "+request.rawHeaders[index+1]+"\r\n");
       forward_socket.write("\r\n");
-      forward_socket.write(head1);
+      forward_socket.write(head);
       socket.pipe(forward_socket);
       forward_socket.pipe(socket);
     });
